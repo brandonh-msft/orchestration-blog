8b6420713ece33546e2b021377cba6da9c524ce6
Brandon H <brandon@bc3tech.net>
2018-05-29 21:46:20 -0700

    fan out/in - parallel execution

--- Classic/Functions/Function1.cs
+++ Classic/Functions/Function1.cs
@@ -1,5 +1,7 @@
 
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -9,48 +11,101 @@ namespace Functions
 {
     public static class Function1
     {
-        private struct Error
+        private class Error
         {
             public int id { get; set; }
             public string message { get; set; }
         };
 
         [FunctionName("Validate")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req, TraceWriter log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req, TraceWriter log)
         {
             var errors = new List<Error>();
 
-            if ((req?.Name?.First?.Length > 1) == false)
-            {
-                errors.Add(new Error { id = 1, message = "First name is null or not longer than 1 character" });
-            }
+            // Fan Out
+            var checks = new List<Task<Error>>
+{
+    CheckFirstName(req, errors),
+    CheckLastName(req, errors),
+    CheckAddressLines(req, errors),
+    CheckCity(req, errors),
+    CheckState(req, errors),
+    CheckZip(req, errors)
+};
 
-            if ((req?.Name?.Last?.Length > 1) == false)
-            {
-                errors.Add(new Error { id = 2, message = "Last name is null or not longer than 1 character" });
-            }
+            // wait for all checks to complete (Fan In)
+            await Task.WhenAll(checks);
 
-            if (!string.IsNullOrEmpty(req?.Address?.Line2) && string.IsNullOrEmpty(req?.Address?.Line1))
-            {
-                errors.Add(new Error { id = 3, message = "Address line 2 is populated but line 1 is empty" });
-            }
-
-            if (string.IsNullOrEmpty(req?.Address?.City))
-            {
-                errors.Add(new Error { id = 4, message = "City is empty" });
-            }
-
-            if ((req?.Address?.State?.Length == 2) == false)
-            {
-                errors.Add(new Error { id = 5, message = "State is null or not a 2-letter abbreviation" });
-            }
-
-            if ((req?.Address?.Zip?.Length == 5) == false)
-            {
-                errors.Add(new Error { id = 6, message = "Zip is null or not 5 digits" });
-            }
+            // Add any non-null values to our errors list
+            errors.AddRange(checks.Select(t => t.Result).Where(r => r != null));
 
             return new OkObjectResult(errors);
+        }
+
+        private static async Task<Error> CheckZip(Person req, List<Error> errors)
+        {
+            await Task.Delay(1000);
+            if ((req?.Address?.Zip?.Length == 5) == false)
+            {
+                return new Error { id = 6, message = "Zip is null or not 5 digits" };
+            }
+
+            return null;
+        }
+
+        private static async Task<Error> CheckState(Person req, List<Error> errors)
+        {
+            await Task.Delay(1100);
+            if ((req?.Address?.State?.Length == 2) == false)
+            {
+                return new Error { id = 5, message = "State is null or not a 2-letter abbreviation" };
+            }
+
+            return null;
+        }
+
+        private static async Task<Error> CheckCity(Person req, List<Error> errors)
+        {
+            await Task.Delay(1200);
+            if (string.IsNullOrEmpty(req?.Address?.City))
+            {
+                return new Error { id = 4, message = "City is empty" };
+            }
+
+            return null;
+        }
+
+        private static async Task<Error> CheckAddressLines(Person req, List<Error> errors)
+        {
+            await Task.Delay(1300);
+            if (!string.IsNullOrEmpty(req?.Address?.Line2) && string.IsNullOrEmpty(req?.Address?.Line1))
+            {
+                return new Error { id = 3, message = "Address line 2 is populated but line 1 is empty" };
+            }
+
+            return null;
+        }
+
+        private static async Task<Error> CheckLastName(Person req, List<Error> errors)
+        {
+            await Task.Delay(1400);
+            if ((req?.Name?.Last?.Length > 1) == false)
+            {
+                return new Error { id = 2, message = "Last name is null or not longer than 1 character" };
+            }
+
+            return null;
+        }
+
+        private static async Task<Error> CheckFirstName(Person req, List<Error> errors)
+        {
+            await Task.Delay(1500);
+            if ((req?.Name?.First?.Length > 1) == false)
+            {
+                return new Error { id = 1, message = "First name is null or not longer than 1 character" };
+            }
+
+            return null;
         }
     }
 }
