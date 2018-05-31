525e8da897152f413adf79b9639c20a9fa5c28c3
Brandon H <hurlburb@microsoft.com>
2018-05-30 21:03:02 -0700

    service bus chaining

--- Classic/Functions/Function1.cs
+++ Classic/Functions/Function1.cs
@@ -1,57 +1,97 @@
-
-using System.Collections.Generic;
+using System;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
-using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Functions
 {
     public static class Function1
     {
-        private class Error
+        public class Error
         {
             public int id { get; set; }
             public string message { get; set; }
         };
 
+        public class ErrorTableEntity : TableEntity
+        {
+            public ErrorTableEntity() { }
+
+            public ErrorTableEntity(Error err, string sessionId)
+            {
+                this.PartitionKey = this.RowKey = sessionId;
+
+                this.ErrorId = err.id;
+                this.ErrorMessage = err.message;
+            }
+
+            public int ErrorId { get; set; }
+
+            public string ErrorMessage { get; set; }
+        }
+
+        public class QueueMessage
+        {
+            public string id { get; set; }
+            public Person data { get; set; }
+        }
+
         private static HttpClient _client = new HttpClient { BaseAddress = new System.Uri(@"http://localhost:7071/api/") };
 
         [FunctionName("Validate")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req, TraceWriter log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req,
+            [ServiceBus(@"function-a", Connection = @"ServiceBusOrchestrationConnection")]out QueueMessage functionAmsg,
+            TraceWriter log)
         {
-            var errors = new List<Error>();
+            var id = Guid.NewGuid().ToString();
+            log.Info($@"New orchestration started: {id}");
+            functionAmsg = new QueueMessage { id = id, data = req };
 
-            var personPostContent = new StringContent(JsonConvert.SerializeObject(req), Encoding.Default, @"application/json");
-            // Fan Out
-            var checks = new List<Task<HttpResponseMessage>>
+            return new AcceptedResult($@"/api/GetResult?id={id}", functionAmsg);
+        }
+
+        [FunctionName("CheckFirstName")]
+        public static void CheckFirstName([ServiceBusTrigger(@"function-a", Connection = @"ServiceBusOrchestrationConnection")]QueueMessage msg,
+            [Table(@"checkfirstnameoutput", Connection = @"AzureWebJobsStorage")]out ErrorTableEntity err,
+            TraceWriter log)
+        {
+            log.Info($@"Message received: {msg.id}");
+            if ((msg?.data?.Name?.First?.Length > 1) == false)
             {
-                _client.PostAsync(@"CheckFirstName", personPostContent),
-                _client.PostAsync(@"CheckLastName", personPostContent),
-                _client.PostAsync(@"CheckAddressLines", personPostContent),
-                _client.PostAsync(@"CheckCity", personPostContent),
-                _client.PostAsync(@"CheckState", personPostContent),
-                _client.PostAsync(@"CheckZip", personPostContent)
-            };
-
-            // wait for all checks to complete (Fan In)
-            var responses = await Task.WhenAll(checks);
-
-            // Add any non-null values to our errors list
-            foreach (var response in responses)
-            {
-                var err = await response.Content.ReadAsAsync<Error>();
-                if (err != null)
-                {
-                    errors.Add(err);
-                }
+                err = new ErrorTableEntity(new Error { id = 1, message = "First name is null or not longer than 1 character" }, msg.id);
+                log.Info($@" - Error logged: {err.ErrorMessage}");
             }
+            else
+            {
+                err = new ErrorTableEntity(new Error { id = 0 }, msg.id);
+                log.Info($@" - NoError logged");
+            }
+        }
 
-            return new OkObjectResult(errors);
+        [FunctionName("GetResult")]
+        public static async Task<IActionResult> GetResultAsync([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]HttpRequest req, [Table(@"checkfirstnameoutput", Connection = @"AzureWebJobsStorage")]CloudTable firstnameoutputTable)
+        {
+            string targetId = null;
+
+            if (req.GetQueryParameterDictionary()?.TryGetValue(@"id", out targetId) == true)
+            {
+                var queryResult = await firstnameoutputTable.ExecuteAsync(TableOperation.Retrieve<ErrorTableEntity>(targetId, targetId));
+                if (queryResult.Result != null)
+                {
+                    return new OkObjectResult(queryResult.Result);
+                }
+
+                return new NotFoundResult();
+            }
+            else
+            {
+                return new BadRequestObjectResult(@"'id' parameter is required");
+            }
         }
 
         [FunctionName("CheckZip")]
@@ -109,18 +149,6 @@ namespace Functions
             if ((req?.Name?.Last?.Length > 1) == false)
             {
                 return new OkObjectResult(new Error { id = 2, message = "Last name is null or not longer than 1 character" });
-            }
-
-            return new NoContentResult();
-        }
-
-        [FunctionName("CheckFirstName")]
-        public static async Task<IActionResult> CheckFirstName([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req)
-        {
-            await Task.Delay(1500);
-            if ((req?.Name?.First?.Length > 1) == false)
-            {
-                return new OkObjectResult(new Error { id = 1, message = "First name is null or not longer than 1 character" });
             }
 
             return new NoContentResult();
