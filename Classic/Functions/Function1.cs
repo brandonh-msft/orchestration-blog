e572ebd4f847bae06c550512e0699f34868540fe
Brandon H <brandon@bc3tech.net>
2018-05-31 09:08:12 -0700

    Durable Functions

--- Classic/Functions/Function1.cs
+++ Classic/Functions/Function1.cs
@@ -1,12 +1,9 @@
-using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Functions
 {
@@ -18,140 +15,123 @@ namespace Functions
             public string message { get; set; }
         };
 
-        public class ErrorTableEntity : TableEntity
-        {
-            public ErrorTableEntity() { }
-
-            public ErrorTableEntity(Error err, string sessionId)
-            {
-                this.PartitionKey = this.RowKey = sessionId;
-
-                this.ErrorId = err.id;
-                this.ErrorMessage = err.message;
-            }
-
-            public int ErrorId { get; set; }
-
-            public string ErrorMessage { get; set; }
-        }
-
-        public class QueueMessage
-        {
-            public string id { get; set; }
-            public Person data { get; set; }
-        }
-
-        private static HttpClient _client = new HttpClient { BaseAddress = new System.Uri(@"http://localhost:7071/api/") };
-
         [FunctionName("Validate")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req,
-            [ServiceBus(@"function-a", Connection = @"ServiceBusOrchestrationConnection")]out QueueMessage functionAmsg,
+        public static async Task<HttpResponseMessage> RunAsync([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequestMessage req,
+            [OrchestrationClient]DurableOrchestrationClient client,
             TraceWriter log)
         {
-            var id = Guid.NewGuid().ToString();
-            log.Info($@"New orchestration started: {id}");
-            functionAmsg = new QueueMessage { id = id, data = req };
+            var personObj = Newtonsoft.Json.JsonConvert.DeserializeObject<Person>(await req.Content.ReadAsStringAsync());
+            var orchestrationInstanceId = await client.StartNewAsync(@"Start", req);
 
-            return new AcceptedResult($@"/api/GetResult?id={id}", functionAmsg);
+            return await client.WaitForCompletionOrCreateCheckStatusResponseAsync(req, orchestrationInstanceId);
+        }
+
+        [FunctionName(nameof(StartAsync))]
+        public static async Task<IList<Error>> StartAsync([OrchestrationTrigger]DurableOrchestrationContext context)
+        {
+            var person = context.GetInput<Person>();
+            var firstNameError = await context.CallActivityAsync<Error>(nameof(CheckFirstName), person);
+
+            return new[] { firstNameError };
         }
 
         [FunctionName("CheckFirstName")]
-        public static void CheckFirstName([ServiceBusTrigger(@"function-a", Connection = @"ServiceBusOrchestrationConnection")]QueueMessage msg,
-            [Table(@"checkfirstnameoutput", Connection = @"AzureWebJobsStorage")]out ErrorTableEntity err,
-            TraceWriter log)
+        public static Error CheckFirstName([ActivityTrigger]DurableActivityContext context, TraceWriter log)
         {
-            log.Info($@"Message received: {msg.id}");
-            if ((msg?.data?.Name?.First?.Length > 1) == false)
+            var person = context.GetInput<Person>();
+            log.Info($@"Message received: {context.InstanceId}");
+            if ((person?.Name?.First?.Length > 1) == false)
             {
-                err = new ErrorTableEntity(new Error { id = 1, message = "First name is null or not longer than 1 character" }, msg.id);
-                log.Info($@" - Error logged: {err.ErrorMessage}");
+                var err = new Error { id = 1, message = "First name is null or not longer than 1 character" };
+                log.Info($@" - Error found: {err.message}");
+                return err;
             }
             else
             {
-                err = new ErrorTableEntity(new Error { id = 0 }, msg.id);
-                log.Info($@" - NoError logged");
+                log.Info($@" - No error found");
+                return null;
             }
         }
 
-        [FunctionName("GetResult")]
-        public static async Task<IActionResult> GetResultAsync([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]HttpRequest req, [Table(@"checkfirstnameoutput", Connection = @"AzureWebJobsStorage")]CloudTable firstnameoutputTable)
-        {
-            string targetId = null;
+        //[FunctionName("GetResult")]
+        //public static async Task<IActionResult> GetResultAsync([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]HttpRequest req, [Table(@"checkfirstnameoutput", Connection = @"AzureWebJobsStorage")]CloudTable firstnameoutputTable)
+        //{
+        //    string targetId = null;
 
-            if (req.GetQueryParameterDictionary()?.TryGetValue(@"id", out targetId) == true)
-            {
-                var queryResult = await firstnameoutputTable.ExecuteAsync(TableOperation.Retrieve<ErrorTableEntity>(targetId, targetId));
-                if (queryResult.Result != null)
-                {
-                    return new OkObjectResult(queryResult.Result);
-                }
+        //    if (req.GetQueryParameterDictionary()?.TryGetValue(@"id", out targetId) == true)
+        //    {
+        //        var queryResult = await firstnameoutputTable.ExecuteAsync(TableOperation.Retrieve<ErrorTableEntity>(targetId, targetId));
+        //        if (queryResult.Result != null)
+        //        {
+        //            return new OkObjectResult(queryResult.Result);
+        //        }
 
-                return new NotFoundResult();
-            }
-            else
-            {
-                return new BadRequestObjectResult(@"'id' parameter is required");
-            }
-        }
+        //        return new NotFoundResult();
+        //    }
+        //    else
+        //    {
+        //        return new BadRequestObjectResult(@"'id' parameter is required");
+        //    }
+        //}
 
-        [FunctionName("CheckZip")]
-        public static async Task<IActionResult> CheckZip([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req)
-        {
-            await Task.Delay(1000);
-            if ((req?.Address?.Zip?.Length == 5) == false)
-            {
-                return new OkObjectResult(new Error { id = 6, message = "Zip is null or not 5 digits" });
-            }
+        //[FunctionName("CheckZip")]
+        //public static async Task<IActionResult> CheckZip([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req)
+        //{
+        //    await Task.Delay(1000);
+        //    if ((req?.Address?.Zip?.Length == 5) == false)
+        //    {
+        //        return new OkObjectResult(new Error { id = 6, message = "Zip is null or not 5 digits" });
+        //    }
 
-            return new NoContentResult();
-        }
+        //    return new NoContentResult();
+        //}
 
-        [FunctionName("CheckState")]
-        public static async Task<IActionResult> CheckState([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req)
-        {
-            await Task.Delay(1100);
-            if ((req?.Address?.State?.Length == 2) == false)
-            {
-                return new OkObjectResult(new Error { id = 5, message = "State is null or not a 2-letter abbreviation" });
-            }
+        //[FunctionName("CheckState")]
+        //public static async Task<IActionResult> CheckState([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req)
+        //{
+        //    await Task.Delay(1100);
+        //    if ((req?.Address?.State?.Length == 2) == false)
+        //    {
+        //        return new OkObjectResult(new Error { id = 5, message = "State is null or not a 2-letter abbreviation" });
+        //    }
 
-            return new NoContentResult();
-        }
+        //    return new NoContentResult();
+        //}
 
-        [FunctionName("CheckCity")]
-        public static async Task<IActionResult> CheckCity([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req)
-        {
-            await Task.Delay(1200);
-            if (string.IsNullOrEmpty(req?.Address?.City))
-            {
-                return new OkObjectResult(new Error { id = 4, message = "City is empty" });
-            }
+        //[FunctionName("CheckCity")]
+        //public static async Task<IActionResult> CheckCity([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req)
+        //{
+        //    await Task.Delay(1200);
+        //    if (string.IsNullOrEmpty(req?.Address?.City))
+        //    {
+        //        return new OkObjectResult(new Error { id = 4, message = "City is empty" });
+        //    }
 
-            return new NoContentResult();
-        }
+        //    return new NoContentResult();
+        //}
 
-        [FunctionName("CheckAddressLines")]
-        public static async Task<IActionResult> CheckAddressLines([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req)
-        {
-            await Task.Delay(1300);
-            if (!string.IsNullOrEmpty(req?.Address?.Line2) && string.IsNullOrEmpty(req?.Address?.Line1))
-            {
-                return new OkObjectResult(new Error { id = 3, message = "Address line 2 is populated but line 1 is empty" });
-            }
+        //[FunctionName("CheckAddressLines")]
+        //public static async Task<IActionResult> CheckAddressLines([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req)
+        //{
+        //    await Task.Delay(1300);
+        //    if (!string.IsNullOrEmpty(req?.Address?.Line2) && string.IsNullOrEmpty(req?.Address?.Line1))
+        //    {
+        //        return new OkObjectResult(new Error { id = 3, message = "Address line 2 is populated but line 1 is empty" });
+        //    }
 
-            return new NoContentResult();
-        }
+        //    return new NoContentResult();
+        //}
 
-        [FunctionName("CheckLastName")]
-        public static async Task<IActionResult> CheckLastName([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req)
-        {
-            await Task.Delay(1400);
-            if ((req?.Name?.Last?.Length > 1) == false)
-            {
-                return new OkObjectResult(new Error { id = 2, message = "Last name is null or not longer than 1 character" });
-            }
+        //[FunctionName("CheckLastName")]
+        //public static async Task<IActionResult> CheckLastName([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]Person req)
+        //{
+        //    await Task.Delay(1400);
+        //    if ((req?.Name?.Last?.Length > 1) == false)
+        //    {
+        //        return new OkObjectResult(new Error { id = 2, message = "Last name is null or not longer than 1 character" });
+        //    }
 
-            return new NoContentResult();
-        }
+        //    return new NoContentResult();
+        //}
     }
 }
